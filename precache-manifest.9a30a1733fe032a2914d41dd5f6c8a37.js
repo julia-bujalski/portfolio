83bebdd459ecadc93110a0a2cd583274e7a8164c
Julia Bujalski <jbujalski@minervaanalytics.co>
2020-08-08 13:29:11 -0400

    Updates

---
+++ precache-manifest.9a30a1733fe032a2914d41dd5f6c8a37.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94ba204a2f93122fbfe6f8eaa26c5479",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "ca417bea41b8251d9d5c",
+    "url": "/portfolio/static/css/main.91b32773.chunk.css"
+  },
+  {
+    "revision": "5287641c0f204359b94a",
+    "url": "/portfolio/static/js/2.ddfca4e1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.ddfca4e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca417bea41b8251d9d5c",
+    "url": "/portfolio/static/js/main.50595a42.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "a308ee131a171b89fa62d763ac63fee9",
+    "url": "/portfolio/static/media/hero_bg.a308ee13.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
