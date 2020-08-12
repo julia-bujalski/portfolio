25c28b3a70721d1f6685a43f0bc7a3202547e84f
Julia Bujalski <jbujalski@minervaanalytics.co>
2020-08-12 11:31:14 -0400

    Updates

---
+++ precache-manifest.05578166abe3226ce3434c07fb08eb26.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e6481a619eedc4ee511c7f9d95689b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6e8166530366a8e46d2",
+    "url": "/static/css/main.ba0857ef.chunk.css"
+  },
+  {
+    "revision": "4661ffb78326c27c2c90",
+    "url": "/static/js/2.6a108248.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6a108248.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6e8166530366a8e46d2",
+    "url": "/static/js/main.bcb97821.chunk.js"
+  },
+  {
+    "revision": "5284eec07972ace79c95",
+    "url": "/static/js/runtime-main.ceaf4b5f.js"
+  },
+  {
+    "revision": "e453d244464c148d16940aac26dd9940",
+    "url": "/static/media/hausily.e453d244.png"
+  },
+  {
+    "revision": "a308ee131a171b89fa62d763ac63fee9",
+    "url": "/static/media/hero_bg.a308ee13.png"
+  },
+  {
+    "revision": "9ece0b9497b56402b1539ef066e77363",
+    "url": "/static/media/introspection.9ece0b94.png"
+  },
+  {
+    "revision": "68a2797b066f9ded5e7a6e034207ef28",
+    "url": "/static/media/quickscope_mockup.68a2797b.png"
+  },
+  {
+    "revision": "dfa3af0bb327687261fef51d11ab51bf",
+    "url": "/static/media/resume.dfa3af0b.png"
+  },
+  {
+    "revision": "8225f6bca23cf9d12d73cc9ef4d90b42",
+    "url": "/static/media/sharpie_watercolor.8225f6bc.png"
+  },
+  {
+    "revision": "b781c91d2c44ac45e8f48ad828e9bcdb",
+    "url": "/static/media/spectra_banner.b781c91d.jpg"
+  },
+  {
+    "revision": "7eda7edaf3d955506bc85797c3b0170b",
+    "url": "/static/media/starlies.7eda7eda.png"
+  }
+]);
