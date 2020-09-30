c05e17598143bc633dc5ec3757fb6f90260e388f
Julia Bujalski <jbujalski@minervaanalytics.co>
2020-09-30 18:08:02 -0400

    add logo process and outcomes to hausily

--- src/views/Hausily/Hausily.js
+++ src/views/Hausily/Hausily.js
@@ -26,6 +26,7 @@ import DesktopMockup from '../../assets/hausily/mockup_desktop.png';
 import MobileMockup from '../../assets/hausily/mockup_mobile.png';
 import Wireframes from '../../assets/hausily/wireframes.png';
 import SiteMap from '../../assets/hausily/site_map.png';
+import LogoProcess from '../../assets/hausily/logo_process.png';
 
 const BlueBackground = `#d6edf7`;
 
@@ -52,8 +53,8 @@ export const Hausily = () => {
               <ProjectTitleWrapper>
                 <SectionTitle>Branding & UI/UX</SectionTitle>
                 <PageHeadline>Hausily</PageHeadline>
-                <BodyText>Cogo Labs <br /> Sept 2018 - Nov 2018</BodyText>
-                <BodyText>A homeowners start up site.</BodyText>
+                <BodyText>Cogo Labs</BodyText>
+                <BodyText>A homeowners start up site</BodyText>
               </ProjectTitleWrapper>
               <FeaturedImage src={ScreensMockup} />
             </FeaturedWrapper>
@@ -62,7 +63,21 @@ export const Hausily = () => {
             <BodyText>
                 As a designer on Cogo's Web Team, I worked as a resource for the rest of the company, including the small incubating start ups that do not have their own designer. I was tasked with creating the branding and site design for Hausily, the early stage start up. The site's goal was to hire data-driven individuals that are interested in creating a web product in the home owner market. Visit <a href="http://www.hausily.com" target="_blank" rel="noopener noreferrer">hausily.com</a> to view the live site.
               </BodyText>
-            <ProjectSectionTitle>Process</ProjectSectionTitle>
+            <ProjectSectionTitle>Logo Design Process</ProjectSectionTitle>
+            <BodyText>
+              With this project, I worked closely with the Hausily team to understand their needs of the logo and site before beginning. With their visuals, they wanted to stay on the profession side - Hausily is the corporate entity not the eventual homeowner product. They wanted a house as the icon to be very obvious and looked towards the Toast branding as inspiration. With these goals, I brainstormed home themes and visuals, tuning into works like habitat, household, abode, base, and house. With this process, I ended up pitching multiple logos to the team, <a href="https://drive.google.com/file/d/1OHyDLwyJL9qJt_5wI67z5-cAODO8Q4o3/view?usp=sharing" target="_blank" rel="noopener noreferrer">click here to view the final logo presentation given to the Hausily Team.</a>
+            </BodyText>
+            <PhotoRow>
+              <ProjectImage src={LogoProcess} />
+            </PhotoRow>
+            <ProjectSectionTitle>Final Style Guide</ProjectSectionTitle>
+            <MockupWrapper backgroundColor={BlueBackground}>
+              <PhotoRow>
+                <ProjectImageC src={HausilyStyleGuide} />
+                <ProjectImageC src={HausilyStyleGuide2} />
+              </PhotoRow>
+            </MockupWrapper>
+            <ProjectSectionTitle>Site Design Process</ProjectSectionTitle>
             <ChallengeSolutionWrapper>
                 <JourneyText>
                   <SectionTitle>Challenge</SectionTitle>
@@ -81,13 +96,6 @@ export const Hausily = () => {
               <ProcessImage src={Wireframes} />
               <ProcessImage src={SiteMap} />
             </MockupWrapper>
-            <ProjectSectionTitle>Style guide</ProjectSectionTitle>
-            <MockupWrapper backgroundColor={BlueBackground}>
-              <PhotoRow>
-                <ProjectImageC src={HausilyStyleGuide} />
-                <ProjectImageC src={HausilyStyleGuide2} />
-              </PhotoRow>
-            </MockupWrapper>
             <ProjectSectionTitle>Final Site Designs </ProjectSectionTitle>
             <SubtitleText>Scroll through mock ups of the Hausily site</SubtitleText>
             <MockupWrapper backgroundColor={BlueBackground}>
@@ -96,6 +104,12 @@ export const Hausily = () => {
                 <LaptopScrollMockup mockup={DesktopMockup} />
               </PhotoRow>
             </MockupWrapper>
+            <ProjectSectionTitle>
+              Outcomes
+            </ProjectSectionTitle>
+            <BodyText>
+              With this project, Hausily was now able to ramp up their recruiting efforts and hire outside of the Cogo affiliation! They were also able to contact and attract investors beyond the the Cogo affiliation. Overall, this project enabled Hausily to be and feel like the independent company they are.
+            </BodyText>
           <ViewMoreProjects />
           </ProjectPageWrapper>
         </PageTransition>
